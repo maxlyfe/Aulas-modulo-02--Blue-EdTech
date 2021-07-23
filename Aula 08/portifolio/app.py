4a33153cb5273abbd1fe0c24ae3f60527ea823c8
maxlyfe <85650989+maxlyfe@users.noreply.github.com>
2021-07-23 09:24:50 -0300

    .

--- Aula 08/portifolio/app.py
+++ Aula 08/portifolio/app.py
@@ -1,4 +1,12 @@
-from flask import Flask,render, request, redirect
+# Dica, crie um gmail especifico para essa atividade, para não compromoter a integridade do seu gmail pessoal.
+
+#Após a criação configure os acesso em seu gmail:
+   # Permitir aplicativos menos seguros (Ativar): https://myaccount.google.com/lesssecureapps
+   # Desbloquear acesso os gmail: https://accounts.google.com/b/2/DisplayUnlockCaptcha
+   # Ativar o acesso via IMAP: https://mail.google.com/mail/#settings/fwdandpop
+# A verificação de duas etapas deve estar desativada, caso o contrário, você precisará configurar uma senha de app e colocar essa senha aqui.
+
+from flask import Flask,render_template, redirect, request 
 from flask_mail import Mail, Message #pip install Flask-Mail
 
 
@@ -9,8 +17,8 @@ mail_settings = {
     "MAIL_PORT": 465,
     "MAIL_USE_TLS": False,
     "MAIL_USE_SSL": True,
-    "MAIL_USERNAME": '', #Seu e-mail
-    "MAIL_PASSWORD": '', #Sua senha
+    "MAIL_USERNAME": 'maxblueedtech@gmail.com', #Seu e-mail
+    "MAIL_PASSWORD": 'max@blue', #Sua senha
 }
 
 app.config.update(mail_settings) #Atualiza as configurações do app
@@ -27,10 +35,10 @@ class Contato:
 def index():
     return render_template('index.html')
 
-@app.route('/send', methods=['POST', 'GET'])
+@app.route('/send', methods=['GET', 'POST'])
 def send():
     if request.method == 'POST':
-        contato = Contato(
+        formContato = Contato(
             request.form['nome'],
             request.form['email'],
             request.form['mensagem']
@@ -39,15 +47,15 @@ def send():
         msg = Message(
             subject='Contato do portfólio',
             sender= app.config.get('MAIL_USERNAME'), #Meu email que vou usar para enviar o formulario
-            recipient=[app.config.get('MAIL_USERNAME')], #Os e-mails que vão receber esta mensagem.
+            recipients=[app.config.get('MAIL_USERNAME'),('maximilianolyfbuzios@gmail.com')], #Os e-mails que vão receber esta mensagem.
             body= f'''
-                {contato.nome} com o e-mail {contato.email}, enviou a seguinte mensagem:
+                {formContato.nome} com o e-mail {formContato.email}, enviou a seguinte mensagem:
                 
-                {contato.mensagem}
+                {formContato.mensagem}
             '''
         )
         mail.send(msg)
-    return render_template('')
+    return render_template('send.html', formContato=formContato)
 
 if __name__ == '__main__':
     app.run(debug=True)
